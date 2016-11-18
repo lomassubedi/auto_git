3f320a86cace135823ef5c9ba7cab1d78315f9ee
lomassubedi <subedilomas@gmail.com>
2016-11-18 11:49:58 +0545

    about call git command from windows console itself.

--- git_redirect.bat
+++ git_redirect.bat
@@ -32,62 +32,60 @@ rem echo %repoDir%
 
 rem append the file name
 set final_path=%repoDir%%script_file_name%
-echo %final_path%
 %final_path%
-pause
 
 
-rem rem Copy path in DOS style 
-rem set unix_path=%final_path% 
+rem Copy path in DOS style 
+set unix_path=%final_path% 
 
-rem rem replace '\' with '/' for unix path conversion
-rem set unix_path=%unix_path:\=/%
+rem replace '\' with '/' for unix path conversion
+set unix_path=%unix_path:\=/%
 
-rem rem Omit ':'
-rem set unix_path=%unix_path::=%
+rem Omit ':'
+set unix_path=%unix_path::=%
 
+rem Omit the white space present
+call :Trim unix_path %unix_path%
+
+rem Add quotation marks at the end
+set unix_path=^"^/%unix_path%^" &rem append '"'
+
+rem Omit the white space present in the ends again
+call :Trim unix_path %unix_path%
+
+rem discard white space at the end and manage to place "\ " instead of 
+rem solve for white space present directory
+set unix_path=%unix_path: =\ %
+rem echo %unix_path%
+
+rem for repo dir
+set dirOnly=%repoDir%
+
+rem replace '\' with '/' for unix path conversion
+set dirOnly=%dirOnly:\=/%
+
+rem Omit ':'
+set dirOnly=%dirOnly::=%
+
+rem Add '/' in the begining
+set dirOnly=^/%dirOnly%^/
 rem rem Omit the white space present
 rem call :Trim unix_path %unix_path%
 
-rem rem Add quotation marks at the end
-rem set unix_path=^"^/%unix_path%^" &rem append '"'
+rem set dirOnly=%unix_path:"=%
+call :Trim dirOnly %dirOnly%
 
-rem rem Omit the white space present in the ends again
-rem call :Trim unix_path %unix_path%
+rem echo %dirOnly%
+echo %dirOnly% >unix_path.dat &rem Write into the repo path to a text file
 
-rem rem discard white space at the end and manage to place "\ " instead of 
-rem rem solve for white space present directory
-rem set unix_path=%unix_path: =\ %
-rem rem echo %unix_path%
+timeout 3
 
-rem rem for repo dir
-rem set dirOnly=%repoDir%
-
-rem rem replace '\' with '/' for unix path conversion
-rem set dirOnly=%dirOnly:\=/%
-
-rem rem Omit ':'
-rem set dirOnly=%dirOnly::=%
-
-rem rem Add '/' in the begining
-rem set dirOnly=^/%dirOnly%^/
-rem rem rem Omit the white space present
-rem rem call :Trim unix_path %unix_path%
-
-rem rem set dirOnly=%unix_path:"=%
-rem call :Trim dirOnly %dirOnly%
-
-rem rem echo %dirOnly%
-rem echo %dirOnly% >unix_path.dat &rem Write into the repo path to a text file
-
-rem timeout 3
-
-rem %sh_exe_path% --login -i -c %unix_path%
-rem pause
+%sh_exe_path% --login -i -c %unix_path%
+pause
 
 
-rem :Trim
-rem 	SetLocal EnableDelayedExpansion
-rem 	set Params=%*
-rem 	for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
-rem 	exit /b
+:Trim
+	SetLocal EnableDelayedExpansion
+	set Params=%*
+	for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+	exit /b
